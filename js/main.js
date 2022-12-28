b11b83ac42754c070a344ce8e746aa5e4cec3802
Zagor-mv <110035296+Zagor-mv@users.noreply.github.com>
2022-12-29 02:06:18 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-var instance = new vidbg(".video",{
+var instance = new vidbg(".video",{ 
+    mp4: "video/world.mp4", // URL or relative path to MP4 video
     webm: "video/world.webm", // URL or relative path to webm video
     poster: "video/poster.jpg", // URL or relative path to fallback image
     overlay: false, // Boolean to display the overlay or not
